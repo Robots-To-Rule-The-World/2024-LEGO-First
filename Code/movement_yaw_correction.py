d7364c7483965c971f05e8bebb45774f9cac0ab9
ntheath <nick@leslieheath.net>
2024-10-06 10:49:00 -0400

    Update movement_yaw_correction.py

--- Code/movement_yaw_correction.py
+++ Code/movement_yaw_correction.py
@@ -58,17 +58,22 @@ async def move_straight(distance, speed):
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(distance),0,velocity=-1*speed)
 
 async def main():
-    # Drive Base 1
+    # Reset Base 1, do not change these lines
     motor_pair.pair(motor_pair.PAIR_1, port.D, port.E)
     motion_sensor.reset_yaw(0)
     await runloop.until(motion_sensor.stable)
-    
-    #await spin_turn_cw(180,100) # (degrees to turn, speed)
-    #await spin_turn_ccw(180,100)
-    await move_straight_corrected(15,500) # (distance, speed) distance is arbitrary, must figure out by experimenation
+
+    #put your code here, use
+    # spin_turn_cw(degrees to turn, speed) to turn clockwise or right
+    # sping_turn_ccw(degrees to turn, speed) to turn counter clockwise or left
+    # move_straight_corrected(distance, speed) to go straight with some correction, good for long straights
+    # note - don't change the speed after you have set your distance! changing the speed will affect your distance 
+
+    #Example:
+    #await move_straight_corrected(15,500) # (distance, speed) distance is arbitrary, must figure out by experimenation
                                           # speed can be fast, but if you change speed it will affect distance, choose speed first
-    await spin_turn_cw(45,200)
-    await move_straight_corrected(16,300)
+    #await spin_turn_cw(45,200)
+    #await move_straight_corrected(16,300)
 
     sys.exit(0)
 
