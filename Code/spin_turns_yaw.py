0a4400110dea55095838fa56d6e79b33ad0e2cca
Ben Acker <benacker@gmail.com>
2024-10-16 17:31:02 -0400

    adds TURN_SPEED and VELOCITY Global Parameters. Changes move_straigh function to move_straight_forward. Adds move_straight_backward function

--- Code/spin_turns_yaw.py
+++ Code/spin_turns_yaw.py
@@ -5,6 +5,10 @@ import runloop, motor_pair, sys
 WHEEL_CIRCUMFERENCE = 17.5
 # input must be in the same unit as WHEEL_CIRCUMFERENCE
 
+# Globals for directional motion and turn speed
+TURN_SPEED = 35
+VELOCITY = 500
+
 def degreesForDistance(distance_cm):
     # Add multiplier for gear ratio if needed
     return int((distance_cm/WHEEL_CIRCUMFERENCE) * 360)
@@ -27,8 +31,12 @@ async def spin_turn_ccw(degrees,speed):
     motion_sensor.reset_yaw(0)
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(0.1), 0)
 
-async def move_straight(distance):
-    await motor_pair.move_for_degrees(motor_pair.PAIR_1, -1*degreesForDistance(distance), 0)
+async def move_straight_forward(distance):
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, -1*degreesForDistance(distance), 0, velocity=VELOCITY)
+    #this is uncorrected, we need a corrective straight function as wheels are getting caught on the board
+
+async def move__straight_backward(distance):
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(distance), 0, velocity=VELOCITY)
     #this is uncorrected, we need a corrective straight function as wheels are getting caught on the board
 
 async def main():
