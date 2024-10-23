ebceec4ae26fb282075ec711d24f029d9785664f
ntheath <nick@leslieheath.net>
2024-10-23 15:16:27 -0700

    Update Jackson_mission_12_whale.py
    removed wait for sensor stability and fixed upload to git hub

--- Code/Jackson_mission_12_whale.py
+++ Code/Jackson_mission_12_whale.py
@@ -1,5 +1,53 @@
-454748495051525354606162634443424140465556575859
-    await runloop.until(motion_sensor.stable)    await move_straight(72,400)    await spin_turn_cw(35,300)    await move_straight(13,500)    await dump()    #return to base    await move_straight(35, -600)    await spin_turn_cw(100,300)    motion_sensor.reset_yaw(0)    motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)    # Drive Base 1async def main():    await move_straight(30,800)    await spin_turn_cw(105,300)    await move_straight(138,1100)    await spin_turn_ccw(20,300)    await move_straight(7,600)
+
+from hub import port, motion_sensor
+import runloop, motor_pair, sys, motor, math, time
+
+# cm, this is a constant for your robot
+WHEEL_CIRCUMFERENCE = 17.5
+# input must be in the same unit as WHEEL_CIRCUMFERENCE
+
+wheel_diameter = 5.6 #cm
+
+def degreesForDistance(distance_cm):
+    # Add multiplier for gear ratio if needed
+    return int((distance_cm/WHEEL_CIRCUMFERENCE) * 360)
+
+#These functions work well with yaw correction
+async def spin_turn_cw(degrees,speed):
+    motion_sensor.reset_yaw(0)
+    motor_pair.move_tank(motor_pair.PAIR_1, speed,-1*speed)
+    while abs(motion_sensor.tilt_angles()[0]*.1) < degrees:
+        dummy = 1
+    motor_pair.stop(motor_pair.PAIR_1)
+    motion_sensor.reset_yaw(0)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(0.1), 0)
+
+async def spin_turn_ccw(degrees,speed):
+    motion_sensor.reset_yaw(0)
+    motor_pair.move_tank(motor_pair.PAIR_1, -1*speed,speed)
+    while abs(motion_sensor.tilt_angles()[0]*.1) < degrees:
+        dummy = 1
+    motor_pair.stop(motor_pair.PAIR_1)
+    motion_sensor.reset_yaw(0)
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(0.1), 0)
+
+async def move_straight(distance, speed):
+    await motor_pair.move_for_degrees(motor_pair.PAIR_1, degreesForDistance(distance),0,velocity=speed)
+
+async def dump():
+    await motor.run_to_absolute_position(port.B,307,400)
+    await motor.run_to_absolute_position(port.B,0,400)
+
+async def main():
+    # Drive Base 1
+    motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
+    motion_sensor.reset_yaw(0)
+    #await runloop.until(motion_sensor.stable)
+
+    await move_straight(72,400)
+    await spin_turn_cw(35,300)
+    await move_straight(13,500)
+    await dump()
 
     #return to base
     await move_straight(35, -600)
@@ -11,6 +59,8 @@
     await move_straight(7,600)
 
 
-SystemExit: 01:02:46 PM: Compiled-------------Traceback (most recent call last):  File "Whale 1", line 66, in <module>  File "Whale 1", line 64, in mainSystemExit: 0
-Getting Started
-API Modules
+
+
+    sys.exit(0)
+
+runloop.run(main())
