182a79ea15b139288532a8979fe6454f647577c7
nvcexploder <benacker@gmail.com>
2024-11-06 17:47:49 -0500

    it worked

--- squid_and_krill.py
+++ squid_and_krill.py
@@ -13,32 +13,57 @@ while ready != True:
     print("waiting")
     wait(3)
 
-# song = ["Eb5/8", "Bb4/8", "F5/8", "Bb4/8", "Eb5/8", "Bb4/8", "G5/16", "Eb5/16", "Bb4/8"]
-# song = ["Eb5/8", "F5/8", "Eb5/8", "F5/8", "Eb5/8", "F5/8", "Eb5/8", "F5/8", "Eb5/8", "F5/8"]
-# hub.speaker.play_notes(song)
-
 #motor definition, ports C&D cuz that's how we roll
 motor_left_C = Motor(Port.C,Direction.COUNTERCLOCKWISE)
 motor_right_D = Motor(Port.D,Direction.CLOCKWISE)
     
 #need distance between wheel touching surface in mm
 b = DriveBase(motor_left_C, motor_right_D, 87, 110)
-# b.use_gyro(True)
 state = b.state()
-print(state)
+
+    
+"""
+                    -
+                    .
+                    -
+                    .
+                    -
+                    .
+               ^    -
+|....|....|....|....| (table edge)
+"""
 
 async def Run():
+    sw = StopWatch()
     b.settings(400)
     await b.straight(140, Stop.HOLD, True)
-    await b.turn(-40, Stop.HOLD, True)
-    await b.straight(400, Stop.HOLD, True) #gets first krill
+    await b.turn(-48, Stop.HOLD, True)
+    await b.straight(370, Stop.HOLD, True) # get squid
+    await b.straight(-100, Stop.HOLD, True) 
+    await b.turn(60, Stop.HOLD, True)
+    await b.straight(280, Stop.HOLD, True)
+    await b.turn(-80, Stop.HOLD, True)
+    await b.straight(460, Stop.HOLD, True)
+    # # begin Anglerfish
+    await b.turn(-45, Stop.HOLD, True) # turn towards anglerfish
+    await b.straight(130, Stop.HOLD, True) # engage the switch (changed from 175)
+    b.settings(100)
     await b.turn(45, Stop.HOLD, True)
-    await b.straight(200, Stop.HOLD, True) #gets second krill
-    await b.turn(40, Stop.HOLD, True)
-    await b.straight(110, Stop.HOLD, True) #gets second krill
-    # b.settings(400, 0, 200, 200)
-    await b.turn(-150, Stop.HOLD, True)
-    await b.straight(110, Stop.HOLD, True)
-    await b.straight(1200, Stop.HOLD, True)
-    await b.turn(-90, Stop.Hold, True)
+    b.settings(400)
+    await b.straight(270, Stop.HOLD, True) # this should stop once the fish is hiding
+    
+
+    await b.turn(-25, Stop.HOLD, True)
+    await b.straight(400, Stop.HOLD, True)
+    await b.turn(-75, Stop.HOLD, True)
+    await b.straight(800, Stop.HOLD, True)
+    sw.pause()
+    end = sw.time()
+    print("run took {} seconds".format(end))
 run_task(Run())
+
+"""_summary_
+create a function that stores the inverse of the given function so you can review back to the original
+
+program 2 and 4 are on laptop 4
+"""
